4cf07af877b158039e6a8fa80f307664c5bf30c9
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-03-04 21:19:02 +0530

    Update Student.java

--- StudentOperations/Student.java
+++ StudentOperations/Student.java
@@ -13,12 +13,11 @@ public class Student {
         this.cgpa = cgpa;
     }
 
-    public String getName() {
-        return name;
+    public String getName() { 
+        return name; 
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name) { 
+        this.name = name; 
     }
 
     public long getPRN() {
