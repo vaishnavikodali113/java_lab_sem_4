a511a9a58668df944320499ed60bb17bd1948807
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-03-04 21:29:47 +0530

    Update StudentOperations.java

--- StudentOperations/StudentOperations.java
+++ StudentOperations/StudentOperations.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
-public class StudentOperations{
-	ArrayList<Student> students;
+public class StudentOperations {
+    private ArrayList<Student> students;
 
-	public StudentOperations(){
-		this.students = new ArrayList<>();
-	}
-	
-	public void addStudent(Student student){
+    public StudentOperations() {
+        this.students = new ArrayList<>();
+    }	
+    public void addStudent(Student student){
 		students.add(student);
 		System.out.println("Student added.");
 	}
