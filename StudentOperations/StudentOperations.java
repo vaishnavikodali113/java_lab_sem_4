6017bb3795b54a2114a37a318cfd904480f50fee
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-03-04 21:38:32 +0530

    Update StudentOperations.java

--- StudentOperations/StudentOperations.java
+++ StudentOperations/StudentOperations.java
@@ -29,4 +29,26 @@ public class StudentOperations {
         return null;
     }
 
+    public Student searchByName(String name) {
+         for (Student student : students) {
+             if (student.getName().equalsIgnoreCase(name)) {
+                 return student;
+            }
+        }
+        return null;
+    }
+
+  public boolean updateStudent(long prn, String newName, String newDept, String newBatch, double newCgpa) {
+        Student student = searchByPRN(prn);
+        if (student != null) {
+            student.setName(newName);
+            student.setDept(newDept);
+            student.setBatch(newBatch);
+            student.setCgpa(newCgpa);
+            return true;
+        }
+        return false;
+    }
+
+	
 }
