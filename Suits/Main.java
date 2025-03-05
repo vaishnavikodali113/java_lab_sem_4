1d580d24d7d97ad656d9a8f39db69cc6e61e3000
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-03-05 19:48:25 +0530

    Update Main.java

--- Suits/Main.java
+++ Suits/Main.java
@@ -5,4 +5,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Deck deck = new Deck();
 
-	
+	while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Print Deck");
+            System.out.println("2. Print a Specific Card");
+            System.out.println("3. Print All Cards of a Suit");
+            System.out.println("4. Print All Cards of a Rank");
+            System.out.println("5. Find a Specific Card");
+            System.out.println("6. Deal 5 Random Cards");
+            System.out.println("7. Shuffle Deck");
+            System.out.println("8. Exit");
+            System.out.print("Choose an option: ");
+            
