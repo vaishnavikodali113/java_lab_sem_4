94e7d48d56a25e125b3e17db55864f7028ca0261
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-03-05 19:44:29 +0530

    Update Card.java

--- Suits/Card.java
+++ Suits/Card.java
@@ -1,13 +1,16 @@
-public class Card{
-	String rank;
-	String suit;
-	
-	public Card(String rank, String suit) {
-		this.rank = rank;
-		this.suit = suit;
-	}
-	
-	public String toString() {
-		return rank + " of "+ suit;
-	}
+// Represents a playing card with a rank and suit
+class Card {
+    String rank;
+    String suit;
+
+    // Constructor to initialize a card with rank and suit
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    // Returns the card as a formatted string
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
