670b6dbf446dcdae555f3099978320a50e6e2214
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-03-05 14:40:43 +0530

    Add files via upload

---
+++ Suits/Card.java
@@ -0,0 +1,13 @@
+public class Card{
+	String rank;
+	String suit;
+	
+	public Card(String rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+	
+	public String toString() {
+		return rank+" of "+suit;
+	}
+}
