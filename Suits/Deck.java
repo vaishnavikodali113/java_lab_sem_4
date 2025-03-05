d2bcbdabd0f8009e961462981785bf177a0e1b9e
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-03-05 19:03:56 +0530

    Update Deck.java

--- Suits/Deck.java
+++ Suits/Deck.java
@@ -50,5 +50,11 @@ public class Deck{
         }
         System.out.println("Card not found.");
     }
-
+	public void dealCard() {
+        System.out.println("Dealing 5 random cards:");
+        Collections.shuffle(deck);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(deck.get(i));
+        }
+    }
 }
