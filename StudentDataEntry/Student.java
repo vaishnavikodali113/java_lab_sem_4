12f217739d6fc5d9eae988ffcd67bb537f762d00
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-04-13 23:56:34 +0530

    Update Student.java

--- StudentDataEntry/Student.java
+++ StudentDataEntry/Student.java
@@ -2,3 +2,9 @@ public class Student {
     private String name;
     private String prn;
     private int age;
+
+    public Student(String name, String prn, int age) {
+        this.name = name;
+        this.prn = prn;
+        this.age = age;
+    }
