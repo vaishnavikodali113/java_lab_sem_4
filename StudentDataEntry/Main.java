8d23588c539b9591a236de3d47a4e0356126ae99
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-04-14 00:08:43 +0530

    Update Main.java

--- StudentDataEntry/Main.java
+++ StudentDataEntry/Main.java
@@ -1,1 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+    static ArrayList<Student> students = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
