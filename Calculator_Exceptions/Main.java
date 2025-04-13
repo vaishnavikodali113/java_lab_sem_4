a3330b47955f44dc549ec6ba701e9c4f1d463f4e
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-04-13 23:00:53 +0530

    Update Main.java

--- Calculator_Exceptions/Main.java
+++ Calculator_Exceptions/Main.java
@@ -1,2 +1,9 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
+public class Main {
+
+    // Calculator functions
+    public static int add(int a, int b) {
+        return a + b;
+    }
