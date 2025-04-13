5b64ff8a05d3b3ba258c6b391c7f5cd662d19219
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-04-14 00:08:58 +0530

    Update Main.java

--- StudentDataEntry/Main.java
+++ StudentDataEntry/Main.java
@@ -4,3 +4,18 @@ import java.util.Scanner;
 public class Main {
     static ArrayList<Student> students = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n--- Student Data Entry ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = Integer.parseInt(sc.nextLine());
