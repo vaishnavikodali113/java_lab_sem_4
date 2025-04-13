6ead7216b5664139f6249117914d655e851a93a9
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-04-14 00:03:57 +0530

    Update CustomExceptions.java

--- StudentDataEntry/CustomExceptions.java
+++ StudentDataEntry/CustomExceptions.java
@@ -4,3 +4,8 @@ class DuplicatePRNException extends Exception {
     }
 }
 
+class InvalidDataException extends Exception {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
