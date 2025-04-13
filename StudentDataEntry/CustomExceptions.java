95e069e5697827d02d1428ef1097a8a96b0cfcdf
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-04-14 00:04:18 +0530

    Update CustomExceptions.java

--- StudentDataEntry/CustomExceptions.java
+++ StudentDataEntry/CustomExceptions.java
@@ -9,3 +9,9 @@ class InvalidDataException extends Exception {
         super(message);
     }
 }
+
+class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
