dda91d5163b1336730faca70b985c067260aa8fa
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-04-13 23:02:31 +0530

    Update Main.java

--- Calculator_Exceptions/Main.java
+++ Calculator_Exceptions/Main.java
@@ -15,3 +15,10 @@ public class Main {
     public static int multiply(int a, int b) {
         return a * b;
     }
+
+    public static int divide(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Cannot divide by zero.");
+        return a / b;
+    }
+
+    
