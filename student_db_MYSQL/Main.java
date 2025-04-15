72efc18ff71be3b2982092d5ce68cb53bddee9ee
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-04-15 23:11:50 +0530

    Update Main.java

--- student_db_MYSQL/Main.java
+++ student_db_MYSQL/Main.java
@@ -1,1 +1,9 @@
+import java.sql.*;
+import java.util.Scanner;
 
+public class Main {
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/student_db_entry";
+    static final String USER = "root";
+    static final String PASS = "student@123";
+
+    static Scanner sc = new Scanner(System.in);
